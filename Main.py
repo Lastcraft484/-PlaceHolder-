6d6d52996987a22184a65ac4d58af1cfd6eedd3a
Aidan Bustos <a@bustos.org>
2020-04-29 16:21:25 -0400

    Moved token to config and reset token
    I don't want to trigger the discord gremlins

--- Main.py
+++ Main.py
@@ -9,4 +9,5 @@ async def ping():
     print(Pong!)
 
 if __name__ == '__main__':
-    bot.run(NTk0NjYxOTI3ODQyNzQyMzA0.XqnhDA.mCceOqHRT9EXPRJ5kzxA_LpRc8o)
+    import config
+    bot.run(config.token)
