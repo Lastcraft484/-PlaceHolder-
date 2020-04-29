5a9c73114558a14577efb913c2ab029eb1af71d9
Aidan Bustos <a@bustos.org>
2020-04-29 16:18:49 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='`')
+bot = commands.Bot(command_prefix='!')
 
 
 @bot.command()
 async def ping():
     print(Pong!)
+
+if __name__ == '__main__':
+    bot.run(NTk0NjYxOTI3ODQyNzQyMzA0.XqnhDA.mCceOqHRT9EXPRJ5kzxA_LpRc8o)
